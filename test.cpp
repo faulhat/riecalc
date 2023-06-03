5023cbbca99401f1136ddd8ffbc35e71b605eec9
Tom Faulhaber <faulhat@wwu.edu>
2023-06-03 13:02:47 -0700

    Minor improvement to test program.

--- test.cpp
+++ test.cpp
@@ -34,7 +34,7 @@ void add_fn(JitRuntime &rt,
 void test_expr(JitRuntime &rt,
                const char *in,
                Table &table,
-               int *ctr,
+               int *ctr, int *fails,
                double expected = 0,
                double delta = 0.001) {
    Expr *expr;
@@ -47,9 +47,9 @@ void test_expr(JitRuntime &rt,
    yyparse(&expr, &funcname, &errc, &errv);
    if (errc != 0) {
       printf("Parser error: %s\n\n", errv[0]);
-      ++*ctr;
+      ++*fails;
       
-      goto cleanup;
+      goto done;
    }
 
    Func fn;
@@ -58,9 +58,9 @@ void test_expr(JitRuntime &rt,
    }
    catch (NameResFail err) {
       err.report();
-      ++*ctr;
+      ++*fails;
       
-      goto cleanup; 
+      goto done; 
    }
 
    result = (*fn)(0);
@@ -70,17 +70,19 @@ void test_expr(JitRuntime &rt,
 
    if (result < expected - delta || result > expected + delta) {
       printf("FAILED! Expected: %f\n\n", expected);
-      ++*ctr;
+      ++*fails;
    }
    else {
       printf("Success!\n\n");
    }
 
-cleanup:
+done:
    yylex_destroy();
    if (expr != nullptr) {
       destroy_expr(expr);
    }
+
+   ++*ctr;
 }
 
 void run_tests() {
@@ -102,13 +104,14 @@ void run_tests() {
       add_fn(rt, functions[i], table);
    }
    
-   int ctr = 0;
-   test_expr(rt, "F(3)", table, &ctr, 7);
-   test_expr(rt, "G(2)", table, &ctr, 20);
-   test_expr(rt, "G(3)^-F(-1)", table, &ctr, 56);
-   test_expr(rt, "E()^(Ln(5) + Ln(2))", table, &ctr, 10);
-   test_expr(rt, "Cos(Pi())", table, &ctr, -1);
-   test_expr(rt, "2[Sin(3 * Pi()/2)]", table, &ctr, 2);
+   int ctr = 0, fails = 0;
+   test_expr(rt, "F(3)", table, &ctr, &fails, 7);
+   test_expr(rt, "G(2)", table, &ctr, &fails, 20);
+   test_expr(rt, "G(3)^-F(-1)", table, &ctr, &fails, 56);
+   test_expr(rt, "E()^(Ln(5) + Ln(2))", table, &ctr, &fails, 10);
+   test_expr(rt, "Cos(Pi())", table, &ctr, &fails, -1);
+   test_expr(rt, "2[Sin(3 * Pi()/2)]", table, &ctr, &fails, 2);
+   printf("%d tests completed. %d failures. %d successes.\n", ctr, fails, ctr - fails);
 
    for (auto f: table) {
       rt.release(*f.second);
