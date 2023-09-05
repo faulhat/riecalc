887fc9e14a362a399d832ce5fd8a103373448b56
Tom Faulhaber <faulhat@wwu.edu>
2023-09-04 18:25:05 -0700

    Made the grapher more interesting.

--- grapher.cpp
+++ grapher.cpp
@@ -3,25 +3,44 @@
 #include <cmath>
 
 gboolean draw(GtkWidget *widget, cairo_t *cr, gpointer data) {
-   static const GdkRGBA RED = {1.0, 0.0, 0.0, 1.0};
+   return ((Grapher *)data)->draw_graph(cr);
+}
 
-   Grapher *parent = (Grapher *)data;
-
-   GtkStyleContext *ctx = gtk_widget_get_style_context(widget);
-   guint width = gtk_widget_get_allocated_width(widget),
-         height = gtk_widget_get_allocated_height(widget);
+gboolean Grapher::draw_graph(cairo_t *cr) {
+   static const GdkRGBA RED = {1.0, 0.0, 0.0, 1.0},
+                        FOG = {0.3, 0.3, 0.3, 1.0},
+                        SKY = {135.0/255.0,
+                               206.0/255.0,
+                               235.0/255.0,
+                               1.0};
+   GtkStyleContext *ctx = gtk_widget_get_style_context(graphing_area);
+   guint width = gtk_widget_get_allocated_width(graphing_area),
+         height = gtk_widget_get_allocated_height(graphing_area);
 
    gtk_render_background(ctx, cr, 0, 0, width, height);
 
-   const Func &fn = parent->getFn();
+   gdk_cairo_set_source_rgba(cr, &SKY);
+   cairo_rectangle(cr, 0, 0, width, height);
+   cairo_fill(cr);
+
+   gdk_cairo_set_source_rgba(cr, &FOG);
+   cairo_move_to(cr, 0, height / 2.0);
+   cairo_line_to(cr, width, height / 2.0);
+   cairo_stroke(cr);
+
+   cairo_move_to(cr, width / 2.0, 0);
+   cairo_line_to(cr, width / 2.0, height);
+   cairo_stroke(cr);
+
    if (fn != nullptr) {
       gdk_cairo_set_source_rgba(cr, &RED);
 
-      for (double i = 0.0; i < width; i += 10.0) {
-         double val = fn(i);
-         cairo_arc(cr, i, val, 5, 0, 2 * G_PI);
-         cairo_fill(cr);
+      for (double x = 0.0; x < width; x += 1.0) {
+         double y = height / 2.0 - fn(x - width / 2.0);
+         cairo_line_to(cr, x, y);
       }
+
+      cairo_stroke(cr);
    }
 
    return FALSE;
@@ -30,11 +49,6 @@ gboolean draw(GtkWidget *widget, cairo_t *cr, gpointer data) {
 void activate(GtkApplication *app, gpointer data) {
    ((Grapher *)data)->activate_graph();
 }
-
-Grapher::Grapher() {
-   app = gtk_application_new("io.github.faulhat.riemann", G_APPLICATION_FLAGS_NONE);
-}
-
 
 void Grapher::apply_expr(const Expr *expr) {
    fn = conv_expr(expr, rt, ectx);
@@ -64,35 +78,53 @@ void Grapher::apply_fn_str(const char *in) {
    if (varname != nullptr) {
       free(varname);
    }
-
    yylex_destroy();
+}
+
+void load_expr(GtkWidget *widget, gpointer data) {
+   ((Grapher *)data)->reload_expr();
+}
+
+void Grapher::reload_expr() {
+   const char *expr_str = gtk_entry_get_text((GtkEntry *)expr_entry);
+   apply_fn_str(expr_str);
 }
 
 void Grapher::activate_graph() {
    window = gtk_application_window_new(app);
    gtk_window_set_title(GTK_WINDOW(window), "Grapher");
-   gtk_window_set_default_size(GTK_WINDOW(window), 1000, 750);
+   gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+
+   grid = gtk_grid_new();
+   gtk_container_add(GTK_CONTAINER(window), grid);
+
+   expr_entry = gtk_entry_new();
+   gtk_entry_set_max_length(GTK_ENTRY(expr_entry), 0);
+   gtk_grid_attach(GTK_GRID(grid), expr_entry, 0, 0, 4, 1);
+   g_signal_connect(G_OBJECT(expr_entry), "activate",
+                    G_CALLBACK(load_expr), this);
+   
+   go_button = gtk_button_new_with_label("Go");
+   gtk_grid_attach(GTK_GRID(grid), go_button, 4, 0, 1, 1);
+   g_signal_connect(G_OBJECT(go_button), "clicked",
+                    G_CALLBACK(load_expr), this);
 
    graphing_area = gtk_drawing_area_new();
-   gtk_widget_set_size_request(graphing_area, 100, 100);
+   gtk_widget_set_size_request(graphing_area, 1000, 500);
+   gtk_grid_attach(GTK_GRID(grid), graphing_area, 0, 1, 5, 1);
    g_signal_connect(G_OBJECT(graphing_area), "draw",
                     G_CALLBACK(draw), this);
 
-   gtk_container_add(GTK_CONTAINER(window), graphing_area);
    gtk_widget_show_all(window);
 }
 
 int Grapher::run(int argc, char **argv) {
-   fn = &sqrt;
+   app = gtk_application_new("io.github.faulhat.riemann", G_APPLICATION_FLAGS_NONE);
    g_signal_connect(app, "activate", G_CALLBACK(activate), this);
 
    int status = g_application_run(G_APPLICATION(app), argc, argv);
    g_object_unref(app);
 
    return status;
-}
-
-const Func &Grapher::getFn() const {
-   return fn;
 }
 
--- grapher.hpp
+++ grapher.hpp
@@ -11,6 +11,9 @@ class Grapher {
 
    /* The components of the grapher */
    GtkWidget *window,
+             *grid,
+             *expr_entry,
+             *go_button,
              *graphing_area;
    
    /* Objects used for the compilation of expressions. */
@@ -21,23 +24,22 @@ class Grapher {
    /* Sets fn to the expression in the given expr object. */
    void apply_expr(const Expr *expr);
 
-public:
-   /* Sets up window and compilation context. */
-   Grapher();
-
    /* Parses a string with an expression and sets fn.
     * Adds an error message to the dialog if the parser produced an error.
     */
    void apply_fn_str(const char *in);
-
+public:
    /* Displays the window and the graph. */
    void activate_graph();
+   
+   /* Reloads expression from entry area */
+   void reload_expr();
 
    /* Runs the graphing program */
    int run(int argc, char **argv);
 
-   /* Getter for the function field */
-   const Func &getFn() const;
+   /* Redraws the graph */
+   gboolean draw_graph(cairo_t *cr);
 };
 
 #endif
